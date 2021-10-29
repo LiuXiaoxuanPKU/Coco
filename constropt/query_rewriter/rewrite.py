6ec1c27b8ceeb49e358ae0b3aeccb12016086c0c
zoey1124 <zoey_1124@berkeley.edu>
2021-10-28 22:32:18 -0700

    draft for chenging

--- constropt/query_rewriter/rewrite.py
+++ constropt/query_rewriter/rewrite.py
@@ -165,7 +165,6 @@ def add_limit_one(q, constraints):
 # =================================================================================================================
 # helper functions for remove distinct
 
-
 def check_join_conditions(table, u_in1, u_in2, alias_to_table):
     '''Check whether there is a join condition that implies joining on two unique columns.'''
     success = True
@@ -183,13 +182,13 @@ def check_join_conditions(table, u_in1, u_in2, alias_to_table):
                         for col in d['eq']:
                             col = unalias(alias_to_table, col)
                             # TODO: temporary handler for constants
-                            success &= col in u_in1 or col in u_in2 or col == '$1'
+                            success &= {col} in u_in1 or {col} in u_in2 or col[0] == '$'
                         break
         else:
             # check that equality is on two unique columns
             for col in table['on']['eq']:
                 col = unalias(alias_to_table, col)
-                success &= col in u_in1 or col in u_in2
+                success &= {col} in u_in1 or {col} in u_in2 or col[0] == '$'
     return success
 
 
@@ -236,10 +235,56 @@ def r_in_to_u_in(r_in, constraints, alias_to_table):
     u_in = set()
     for constraint in constraints:
         if constraint.table == r_in and isinstance(constraint, UniqueConstraint):
-            u_in.add(constraint.table + '.' + constraint.field)
-            u_in.add(constraint.field)
+            unique_lst = [constraint.field] + constraint.scope
+            unique_set = set(unique_lst + [constraint.table + '.' + col for col in unique_lst])
+            u_in.add(unique_set)
     return u_in
 
+# SELECT customerName, customercity, customermail, salestotal
+# FROM onlinecustomers AS oc
+#    INNER JOIN
+#    orders AS o
+#    ON oc.customerid = o.customerid
+#    INNER JOIN
+#    sales AS s
+#    ON o.orderId = s.orderId
+#    WHERE s.id = 1
+
+# where (a = 1 or b = 9) and (c = 8 or d = 7)
+def u_in_after_filter(q, u_in, alias_to_table):
+    # check if q has where clause
+    if not 'where' in q:
+        return
+    # check only AND in where clause
+    where = q["where"]
+    if not ("and" in where or "eq" in where):
+        return
+    if "and" in where:
+        where_conditions = where["and"]
+        for cond in where_conditions:
+            # only take care field = constant
+            if "eq" in cond and "$" == cond["eq"][1][0]:
+                cond_column = cond["eq"][0]
+                # remove cond_column from each subset in u_in
+                for subset in u_in:
+                    # id, table_name.id, t.id -> remove them all
+                    cond_column = unalias(cond_column) # table_name.id, id
+                    subset.discard(cond_column)
+                    subset.discard(cond_column.split(".")[-1])
+
+    # only one condition in where
+    elif "eq" in where and "$" == where["eq"][1][0]:
+        cond_column = where["eq"][0]
+        # remove cond_column from each subset in u_in
+        for subset in u_in:
+            # id, table_name.id, t.id -> remove them all
+            cond_column = unalias(cond_column)  # table_name.id, id
+            subset.discard(cond_column)
+            subset.discard(cond_column.split(".")[-1])
+
+
+# select id from a where project_id = 1
+# [id, project_id]
 
 def query_to_u_out(q, constraints, alias_to_table):
     '''Gets the set of unique columns U_out after going through the entire query, save for projections.'''
@@ -249,6 +294,8 @@ def query_to_u_out(q, constraints, alias_to_table):
         tables = [tables]
     r_in1 = tables[0]
     u_in1 = r_in_to_u_in(r_in1, constraints, alias_to_table)
+    # single table case filter
+    u_in_after_filter(q, u_in1, alias_to_table)
     for t in tables[1:]:
         if 'inner join' in t or 'join' in t:
             # get table 2 and its unique set
@@ -257,6 +304,7 @@ def query_to_u_out(q, constraints, alias_to_table):
             else:
                 r_in2 = t['join']
             u_in2 = r_in_to_u_in(r_in2, constraints, alias_to_table)
+            u_in_after_filter(q, u_in2, alias_to_table)
             # check fail: u_out is empty set. else, u_out is union of u_in1 and u_in2.
             if check_join_conditions(t, u_in1, u_in2, alias_to_table):
                 u_in1 = u_in1.union(u_in2)
@@ -269,9 +317,12 @@ def query_to_u_out(q, constraints, alias_to_table):
             else:
                 r_in2 = t['left join']
             u_in2 = r_in_to_u_in(r_in2, constraints, alias_to_table)
+            u_in_after_filter(q, u_in2, alias_to_table)
             # check fail: u_out is empty set. else, u_out is u_in1.
             if not check_join_conditions(t, u_in1, u_in2, alias_to_table):
                 u_in1 = set()
+        # deal with filter after join
+        u_in_after_filter(q, u_in1, alias_to_table)
     return u_in1
 
 
@@ -318,8 +369,10 @@ def remove_distinct_projection(q, u_in, alias_to_table):
             table_dot = dealias_dot = val[0:-1]
             if val[0:-2] in alias_to_table:
                 dealias_dot = alias_to_table[val[0:-2]] + '.'
-            for col in u_in:
-                if table_dot in col or dealias_dot in col:
+            # for col in u_in:
+            for subset in u_in:
+                if table_dot in subset[0] or dealias_dot in subset[0]:
+                # if table_dot in col or dealias_dot in col:
                     rewrite_q = q.copy()
                     rewrite_q['select'] = val
                     return True, rewrite_q
@@ -327,9 +380,12 @@ def remove_distinct_projection(q, u_in, alias_to_table):
             rewrite_q = q.copy()
             rewrite_q['select'] = val
             return True, rewrite_q
+        # {'value': 'name'} case: check if {name} in u_in
     elif isinstance(projections, list):
-        for d in projections:
-            if unalias(alias_to_table, d['value']) in u_in:
+    # [{'value': 'users.name'}, {'value': 'projects.id'}]
+        proj_set = set([unalias(alias_to_table, d['value']) for d in projections])
+        for subset in u_in:
+            if subset <= proj_set:
                 rewrite_q = q.copy()
                 rewrite_q['select'] = projections
                 return True, rewrite_q
@@ -424,5 +480,5 @@ if __name__ == "__main__":
         print(sql.strip())
         sql_obj = parse(sql.strip())
         print(json.dumps(sql_obj, indent=4))
-        rewrite_single_query(sql_obj, constraints)
+        # rewrite_single_query(sql_obj, constraints)
     # TODO : EXISTS, ANY
