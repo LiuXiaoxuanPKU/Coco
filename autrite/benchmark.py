2086231121dd8c52ef05dfb0c1495d9213331d15
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2022-02-17 07:19:52 +0000

    fix for benchmark

--- autrite/benchmark.py
+++ autrite/benchmark.py
@@ -1,17 +1,22 @@
+from cmath import exp
 import random
 
 from fileinput import filename
 from loader import Loader
 from evaluator import Evaluator
 import numpy as np
+from utils import exp_recorder
 
-CONNECT_STRING = "user=redmine password=my_password"
+CONNECT_STRING = "user=redmine password=my_password dbname=redmine_develop"
 
 def generate_query_params(queries):
     def get_field_value(table_field):
+        if table_field.startswith("LOWER"):
+            table_field = table_field[6:-1]
         table, field = table_field.split('.')
         SQL = "SELECT %s FROM %s ORDER BY RANDOM() LIMIT 1" % (table_field, table)
-        return Evaluator.evaluate_query(SQL, CONNECT_STRING)
+        ret =  Evaluator.evaluate_query(SQL, CONNECT_STRING)
+        return ret
 
     def generate_single(q):
         if '$' not in q:
@@ -20,12 +25,34 @@ def generate_query_params(queries):
         for i, token in enumerate(tokens):
             if '$' in token:
                 if tokens[i-1] == "LIMIT":
-                    tokens[i] = str(random.randint(0, 100))
-                elif tokens[i-1] == "=":
+                    tokens[i] = str(random.randint(0, 10))
+                elif tokens[i-1] == "OFFSET":
+                    tokens[i] = "1"
+                elif tokens[i-1] in ["=", '!=', '>', '<']:
                     re = get_field_value(tokens[i-2])
-                    if len(re) == 0:
+                    if len(re) == 0 or re[0][0] is None:
                         return None
-                    tokens[i] = re[0]
+                    tokens[i] = re[0][0]
+                    if isinstance(tokens[i], str):
+                        tokens[i] = "'" + tokens[i] + "'"
+                    else:
+                        tokens[i] = str(tokens[i])
+                elif tokens[i-1] == "IN":
+                    end_idx = i
+                    while ")" not in tokens[end_idx]:
+                        end_idx += 1
+                    for j in range(i, end_idx + 1):
+                        re = get_field_value(tokens[i-2])
+                        if len(re) == 0 or re[0][0] is None:
+                            return None
+                        tokens[j] = re[0][0]
+                        if isinstance(tokens[j], str):
+                            tokens[j] = "'" + tokens[j] + "'"
+                        else:
+                            tokens[j] = str(tokens[j])
+                        tokens[j] += ","
+                    tokens[i] = "(" + tokens[i]
+                    tokens[end_idx] =  tokens[end_idx][:-1] + ")"
         q = " ".join(tokens)
         return q
 
@@ -39,8 +66,16 @@ def generate_query_params(queries):
 def get_slow_queries(queries, ratio):
     ts = []
     for q in queries:
-        ts.append(Evaluator.evaluate_actual_time(q, connect_string=CONNECT_STRING))
-    percentile_t = np.percentile(ts, 1-ratio)
+        if len(q) < 5:
+            ts.append(-1)
+            continue
+        try:
+            ts.append(Evaluator.evaluate_actual_time(q, connect_string=CONNECT_STRING))
+        except:
+            print("[Fail to execute]", q)
+            ts.append(-1)
+    ts_array = np.array(ts)
+    percentile_t = np.percentile(ts_array[ts_array>0], (1 - ratio)*100)
     slow_queries = []
     slow_ts = []
     for i, t in enumerate(ts):
@@ -51,7 +86,12 @@ def get_slow_queries(queries, ratio):
 
 
 if __name__ == "__main__":
-    filename = "../queries/redmine.sql"
-    queries = Loader.load_queries_raw(filename, cnt=100)
+    filename = "../queries/redmine.pk"
+    queries = Loader.load_queries_raw(filename, cnt=1000)
     queries = generate_query_params(queries)
-    get_slow_queries(queries, 0.1)
+    slow_queries, slow_ts = get_slow_queries(queries, 0.1)
+    
+    for q,t in zip(slow_queries, slow_ts):
+        exp_recorder.record("time(ms)", t)
+        exp_recorder.record("queries", q)
+        exp_recorder.dump("log/slow_queries")
