11c16c84202f64a61c599718ee59df97a678ece6
zoey1124 <zoey_1124@berkeley.edu>
2022-03-11 15:41:26 -0800

    fix state machine extraction

--- constropt/constr_extractor/state_machine_extractor.rb
+++ constropt/constr_extractor/state_machine_extractor.rb
@@ -1,5 +1,6 @@
 require_relative './constraint'
 require_relative './base_extractor'
+require_relative './ast_handler'
 
 # extract inclusion constraints with state machine type
 class StateMachineExtractor < Extractor
@@ -11,24 +12,31 @@ class StateMachineExtractor < Extractor
     constraints = []
     ast[2].children.select.each do |c|
       if c.type.to_s == 'command' && c[0].source == 'state_machine'
-        constraints += extract_state_machine_inclusion(c)
+        constraints << extract_state_machine_inclusion(c)
       end
     end
     node.constraints += constraints
+    puts constraints.length
   end
 
-  def extract_state_machine_inclusion(ast) 
+  # begin parsing state machine inclusion constraint
+  def extract_state_machine_inclusion(ast)
+    # get column name
+    column = handle_symbol_literal_node(ast[1][0])
+
+    # get state values
     possible_values = []
     ast[2].children[0].each do |c|
-      possible_values += parse_cmd_get_fields(c)
+      possible_values += parse_state_values(c)
     end
-      constraint = InclusionConstraint.new(column, possible_values, 'state_machine')
+    constraint = InclusionConstraint.new(column, possible_values, 'state_machine')
     constraint
   end
 
-  def parse_cmd_get_fields(cmd)
+  # return state values from state mahine
+  def parse_state_values(cmd)
     possible_fields = []
-    if cmd.children[0].source == "event"
+    if cmd.children[0].source == 'event'
       possible_fields += parse_event_cmd(cmd)
     end
   
@@ -41,4 +49,34 @@ class StateMachineExtractor < Extractor
     end
     possible_fields
   end
+
+  # parse event 
+  def parse_event_cmd(ast)
+    possible_fields = []
+    ast = ast.jump(:do_block)
+    if ast.children.length != 1
+      puts "[Error] even do block can only have one transition " + ast.children.to_s
+    end
+    ast = ast.children[0][0]
+    if ast[0].source == "transition"
+      # transition available: :stopped
+      ast = ast[1].jump(":assoc")[0]
+      ast.each do |assoc|
+        field1 = assoc[0].source if assoc[0].type.to_s == "vcall" # any
+        field1 ||= handle_label_node(assoc[0]) if assoc[0].type.to_s == "label"
+        next if field1 == "if"
+        if field1 != "from" && field1 != "to" && field1 != "any"
+          possible_fields << field1
+        end
+        if assoc[1].type.to_s == "array"
+          fields = handle_array_node(assoc[1])
+          possible_fields += fields if !fields.nil?
+        elsif assoc[1].type.to_s == "symbol_literal"
+          possible_fields << handle_symbol_literal_node(assoc[1])
+        end
+      end
+    end
+    possible_fields
+  end
+
 end
