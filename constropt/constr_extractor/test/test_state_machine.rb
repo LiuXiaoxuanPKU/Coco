2f3d075c5b53d700413985edf2c7acd3bbeba283
zoey1124 <zoey_1124@berkeley.edu>
2022-03-30 13:00:13 -0700

    change puts to raise for detecting errors

--- constropt/constr_extractor/test/test_state_machine.rb
+++ constropt/constr_extractor/test/test_state_machine.rb
@@ -5,25 +5,29 @@ require_relative '../state_machine_extractor'
 require_relative '../class_node'
 require 'yard'
 
-# class TestPrint
-#   def visit(node, _params)
-#     puts "#{node.name} => #{node.constraints.length}, table #{node.table}"
-#   end
-# end
 
-# def test_gitlab 
-#   engine = Engine.new('./data/gitlab_models')
-#   root = engine.run
+class TestPrint
+  def visit(node, _params)
+    puts "#{node.name} => #{node.constraints.length}, table #{node.table}"
+  end
+end
 
-#   t = Traversor.new(StateMachineExtractor.new)
-#   t.traverse(root)
+def test_gitlab 
+  engine = Engine.new('./data/gitlab_models')
+  root = engine.run
 
-#   t = Traversor.new(TestPrint.new)
-#   t.traverse(root)
-# end
+  t = Traversor.new(StateMachineExtractor.new)
+  t.traverse(root)
 
+  t = Traversor.new(TestPrint.new)
+  t.traverse(root)
+end
+
+# uncomment the following if want to run through an application
 # test_gitlab
 
+
+############################## unit tests #################################
 def test_naive 
   class_def = <<-FOO
     class Test
@@ -31,7 +35,6 @@ def test_naive
         event :done do
           transition [:pending] => :done
         end
-        
         state :pending
         state :done
       end
@@ -41,8 +44,12 @@ def test_naive
   node.ast = YARD::Parser::Ruby::RubyParser.parse(class_def).root[0]
   state_machine_extractor = StateMachineExtractor.new
   state_machine_extractor.visit(node, {})
-  puts "# of extracted constraints: #{node.constraints.length}"
-  puts "extracted constraints: #{node.constraints}"
+  # check for possible errors
+  raise "expect 1 constraint, get #{node.constraints.length} constraints" unless node.constraints.length == 1
+
+  if node.constraints[0].values != %w[done pending]
+    raise "values should be ['done', 'pending'], but get #{node.constraints[0].values}"
+  end
 end
 
 def test_naive2
@@ -83,8 +90,12 @@ def test_naive2
   node.ast = YARD::Parser::Ruby::RubyParser.parse(class_def).root[0]
   state_machine_extractor = StateMachineExtractor.new
   state_machine_extractor.visit(node, {})
-  puts "# of extracted constraints: #{node.constraints.length}"
-  puts "extracted constraints: #{node.constraints}"
+  # check for possible errors
+  raise "expect 1 constraint, get #{node.constraints.length} constraints" unless node.constraints.length == 1
+
+  if node.constraints[0].values != %w[created running blocked success failed canceled skipped]
+    raise "values should be ['created', 'running', 'blocked', 'success', 'failed', 'canceled', 'skipped'], but get #{node.constraints[0].values}"
+  end
 end
 
 test_naive
