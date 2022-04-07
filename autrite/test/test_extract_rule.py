1aac288f1856b8a119d7ac6e95ed047652cb9d0c
zoey1124 <zoey_1124@berkeley.edu>
2022-04-07 00:36:18 -0700

    more and more tests

--- autrite/test/test_extract_rule.py
+++ autrite/test/test_extract_rule.py
@@ -8,6 +8,13 @@ import unittest
 
 class TestInclusionConstraintQuery(unittest.TestCase):
 
+    def test_non_example_naive(self):
+        cs = [InclusionConstraint("auth_sources", "fake_column", [])]
+        q = "SELECT 1 AS one FROM auth_sources LIMIT 1"
+        q = parse(q)
+        extracted = ExtractInclusionRule(cs).apply(q)
+        self.assertTrue(len(extracted) == 0)
+
     def test_where_naive(self):
         cs = [InclusionConstraint("attachments", "disk_filename", [])]
         q = "SELECT 1 AS one FROM attachments WHERE disk_filename = '060719210727_archive.zip'"
@@ -46,6 +53,13 @@ class TestInclusionConstraintQuery(unittest.TestCase):
     def test_order_by(self):
         cs = [InclusionConstraint("attachments", "butterfly", [])]
         q = "SELECT DISTINCT attachments.filename AS alias_0, projects.id FROM projects LEFT OUTER JOIN attachments ON attachments.container_id = projects.id AND attachments.container_type = 1 WHERE projects.id = 2 ORDER BY attachments.butterfly ASC LIMIT 3"
+        q = parse(q)
+        extracted = ExtractInclusionRule(cs).apply(q)
+        self.assertTrue(len(extracted) == 1)
+
+    def test_subquery(self):
+        cs = [InclusionConstraint("projects", "id", [])]
+        q = "SELECT 1 AS one FROM enabled_modules WHERE enabled_modules.project_id IN (SELECT projects.id FROM projects WHERE projects.status <> 9 AND projects.is_public = True AND projects.id NOT IN (SELECT project_id FROM members WHERE user_id IN (74, 12))) AND enabled_modules.name = 1 LIMIT 2"
         q = parse(q)
         extracted = ExtractInclusionRule(cs).apply(q)
         self.assertTrue(len(extracted) == 1)
