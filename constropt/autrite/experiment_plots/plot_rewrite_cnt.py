cad8dfdfba0328de233203e6af20d58cf877eb25
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2022-09-13 20:49:11 +0000

    update numbers

--- constropt/autrite/experiment_plots/plot_rewrite_cnt.py
+++ constropt/autrite/experiment_plots/plot_rewrite_cnt.py
@@ -2,51 +2,51 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 redmine = [
-    2861, # qs with constraints
-    1830, # qs with rewritten
-    1464, # qs with lower cost
-    1294, # qs with lower cost and pass tests
-    286   # qs get verified
+    2950, # qs with constraints
+    1674, # qs with rewritten
+    1208, # qs with lower cost
+    1103, # qs with lower cost and pass tests
+    124   # qs get verified
 ]
 
 forem = [
-   4411, # qs with constraints
-   461, # qs with rewritten
-   165, # qs with lower cost
-   124, # qs with lower cost and pass tests
+   4772, # qs with constraints
+   381, # qs with rewritten
+   159, # qs with lower cost
+   122, # qs with lower cost and pass tests
    29   # qs get verified 
 ]
 
 openproject = [
-   10676, # qs with constraints
-   8817, # qs with rewritten
-   4687, # qs with lower cost
-   4329, # qs with lower cost and pass tests
-   709   # qs get verified 
+   13125, # qs with constraints
+   8262, # qs with rewritten
+   0, # qs with lower cost
+   0, # qs with lower cost and pass tests
+   0   # qs get verified 
 ]
 
 spree = [
-   1143, # qs with constraints
-   3503, # qs with rewritten
-   1616, # qs with lower cost
-   149, # qs with lower cost and pass tests
-   12   # qs get verified 
+   1163, # qs with constraints
+   656, # qs with rewritten
+   593, # qs with lower cost
+   561, # qs with lower cost and pass tests
+   459   # qs get verified 
 ]
 
 mastodon = [
-   6936, # qs with constraints
-   1040, # qs with rewritten
-   345, # qs with lower cost
-   345, # qs with lower cost and pass tests
-   22   # qs get verified 
+   6955, # qs with constraints
+   984, # qs with rewritten
+   803, # qs with lower cost
+   435, # qs with lower cost and pass tests
+   65   # qs get verified 
 ]
 
 openstreetmap = [
-   4830, # qs with constraints
-   170, # qs with rewritten
-   131, # qs with lower cost
-   102, # qs with lower cost and pass tests
-   17   # qs get verified 
+   4836, # qs with constraints
+   136, # qs with rewritten
+   101, # qs with lower cost
+   76, # qs with lower cost and pass tests
+   25   # qs get verified 
 ]
 
 fig, ax = plt.subplots()
@@ -73,8 +73,8 @@ ax.axvline(3, alpha=0.2, color='#713b28')
 ax.axvline(4, alpha=0.2, color='#713b28')
 ax.axvline(5, alpha=0.2, color='#713b28')
 
-plt.ylabel("Number of queries", size=15)
-plt.xlabel("Stages of ConstrOpt Rewrite", size=15)
+plt.ylabel("Number of queries", size=17)
+plt.xlabel("Stages of ConstrOpt Rewrite", size=17)
 plt.text(1.4, 8, "Queries with\nconstraints", ha='center',size=12, va='center', weight='bold')
 plt.text(2.5, 8, "Enumerate", ha='center',size=12, va='center', weight='bold')
 plt.text(3.5, 8, "Remove\nslow rewrites", ha='center',size=11, va='center', weight='bold')
@@ -87,4 +87,4 @@ plt.yscale('log', base=10)
 plt.legend(prop={'size': 10}, loc='upper right')
 # plt.subplots_adjust(top=0.92, bottom=5, left=0.10, right=0.95, hspace=0.25,
 #                     wspace=0.35)
-plt.savefig("/home/ubuntu/ConstrOpt/figures/7.3/rewrite_cnt.png")
+plt.savefig("/home/ubuntu/ConstrOpt_warning_suppress/figures/7.3/rewrite_cnt.png")
