5a0c7f1892e6339f70a1f195dcde8fe051eebaed
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2022-11-10 05:29:52 +0000

    Make sub-flakes work.

--- flake.nix
+++ flake.nix
@@ -4,8 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    constropt-extractor.url = "path:./extractor";
-    constropt-rewriter.url = "path:./rewriter";
+    get-flake.url = "github:ursi/get-flake";
+    #constropt-extractor.url = "path:./extractor";
+    #constropt-rewriter.url = "path:./rewriter";
     cosette-parser.url = "git+file:./cosette-parser";
     cosette-prover.url = "git+file:./cosette-prover";
   };
@@ -14,13 +15,16 @@
     self,
     nixpkgs,
     flake-utils,
-    constropt-extractor,
-    constropt-rewriter,
+    get-flake,
+    #constropt-extractor,
+    #constropt-rewriter,
     cosette-parser,
     cosette-prover,
   }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      constropt-extractor = get-flake ./extractor;
+      constropt-rewriter = get-flake ./rewriter;
     in {
       devShell = pkgs.mkShell {
         buildInputs = [
